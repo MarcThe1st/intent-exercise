da71e04700838270cc4dbd7a9cb9989ac8eb15b4
MarcThe1st <113324828+MarcThe1st@users.noreply.github.com>
2022-10-03 12:52:36 +0700

    Menambahkan Intent

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,57 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private CircleImageView foto;
+    private TextView fullname;
+    private TextView email;
+    private TextView about;
+    private TextView homepage;
+    private Button tombol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Bundle extra = getIntent().getExtras();
+        byte[] byteArray = extra.getByteArray("img");
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        foto = findViewById(R.id.image_profile);
+        foto.setImageBitmap(bmp);
+        String nama = getIntent().getExtras().getString("nama");
+        String imel = getIntent().getExtras().getString("email");
+        String tentang = getIntent().getExtras().getString("about");
+        final String rumahpage = getIntent().getExtras().getString("homepage");
+        fullname = findViewById(R.id.label_fullname);
+        email = findViewById(R.id.label_email);
+        about = findViewById(R.id.label_about);
+        homepage = findViewById(R.id.label_homepage);
+        fullname.setText(nama);
+        email.setText(imel);
+        about.setText(tentang);
+        homepage.setText(rumahpage);
+        tombol = findViewById(R.id.button_homepage);
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent website = new Intent();
+                website.setAction(Intent.ACTION_VIEW);
+                website.addCategory(Intent.CATEGORY_BROWSABLE);
+                website.setData(Uri.parse(Uri.decode(rumahpage)));
+                startActivity(website);
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,14 +1,78 @@
 package id.ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+    public static final int camera_code = 69;
+    private Button btn;
+    private ImageView edit;
+    private CircleImageView gambar;
+    private EditText fullname;
+    private EditText email;
+    private EditText homepage;
+    private EditText about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fullname = findViewById(R.id.text_fullname);
+        email = findViewById(R.id.text_email);
+        homepage = findViewById(R.id.text_homepage);
+        about = findViewById(R.id.text_about);
+        gambar = findViewById(R.id.image_profile);
+        edit = findViewById(R.id.imageView);
+        btn = findViewById(R.id.button_ok);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gambar.buildDrawingCache();
+                Bitmap bmp = ((BitmapDrawable)gambar.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.PNG, 50, baos);
+                byte[] byteArray = baos.toByteArray();
+                Intent ganti = new Intent(RegisterActivity.this, ProfileActivity.class);
+                ganti.putExtra("img", byteArray);
+                ganti.putExtra("nama", fullname.getText().toString());
+                ganti.putExtra("email", email.getText().toString());
+                ganti.putExtra("about", about.getText().toString());
+                ganti.putExtra("homepage", homepage.getText().toString());
+                startActivity(ganti);
+            }
+        });
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent foto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(foto, camera_code);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == camera_code){
+                Bitmap cameraimage = (Bitmap) data.getExtras().get("data");
+                gambar.setImageBitmap(cameraimage);
+            }
+        }
     }
 }
